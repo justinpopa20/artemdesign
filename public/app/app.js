5404a49c10f3e2088474559bf74808eb35109340
Popa Iustinian <justin.popa20@gmail.com>
2015-09-20 20:22:29 +0300

    Full page img background added

--- public/app/app.js
+++ public/app/app.js
@@ -3,10 +3,17 @@
     'use strict';
 
     angular.module('artemdesign', ['ngRoute'])
-        .config(['$routeProvider', function($routeProvider) {
+        .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+            $locationProvider.html5Mode({
+                enabled: true,
+                requireBase: false
+            });
             $routeProvider
                 .when('/', {
                     templateUrl: 'app/pages/home.tmpl.html'
+                })
+                .when('/manifesto', {
+                    templateUrl: 'app/pages/manifesto.tmpl.html'
                 })
                 .when('/contact', {
                     templateUrl: 'app/pages/contact.tmpl.html'
