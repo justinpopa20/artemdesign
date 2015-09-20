2745095cd18c627d6363aa57c430d94ae651ab71
Popa Iustinian <justin.popa20@gmail.com>
2015-09-20 17:50:03 +0300

    Implemented menu button when expanded

--- js/menu.js
+++ js/menu.js
@@ -6,16 +6,20 @@
         if (footer.hasClass("active")) {
             newHeight = 244;
         }
+
         var list = footer.find(".list");
         var description = footer.find(".description");
         list.removeClass("hidden");
         description.addClass("hidden");
-        $("footer").animate({
+        footer.addClass("animating");
+
+        footer.animate({
                 height: newHeight
             }, // what we are animating
             'fast', // how fast we are animating
             'swing', // the type of easing
             function() { // the callback
+                footer.removeClass("animating");
                 if (footer.hasClass("active")) {
                     list.addClass("hidden");
                     footer.removeClass("active");
